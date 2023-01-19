85c9bc3010f6d6956c935cf2b28fdbf874ee60c0
Jessyca Toselli <jfamedeiros@gmail.com>
2023-01-19 16:34:13 -0300

    Day Two

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import Fastify from "fastify/fastify.js";
-import { PrismaClient } from "@prisma/client";
+
 import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
